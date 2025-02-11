ea0d6eb5870a5b8f082fe7bc470078f38cc5b106
Loris Moine <loris.moine1402@gmail.com>
2025-02-11 08:58:06 +0100

    New Images

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,2 @@
-let boutons = document.getElementsByClassName('card');
-let premiereImage = document.querySelector('.card img');
 
-function cardSelect() {
-    premiereImage.style.opacity = 1;
-}
-
-for (let bouton of boutons) {
-    bouton.addEventListener('click', cardSelect);
-}
 
