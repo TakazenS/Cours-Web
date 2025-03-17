91a07b9f74b7eee3f4c413b47a96390a2a3b020f
Loris Moine <loris.moine1402@gmail.com>
2025-03-17 08:47:26 +0100

    New JS Exercise

--- Docs/TP-8/script/script-TP-8.js
+++ Docs/TP-8/script/script-TP-8.js
@@ -1,1 +1,28 @@
+let negative = 0;
+let x = '';
+let y = '';
+while (negative != 1) {
+    x = parseFloat(prompt('Entrez une valeur pour la longueur'));
+    y = parseFloat(prompt('Entrez une valeur pour la largeur'));
+    if (x >= 0 && y >= 0) {
+        negative = 1;
+        break;
+    }
+    negativeValue = alert('Vous ne pouvez pas entrer des nombres négatifs !')
+}
 
+let question = confirm(`Ces valeurs vous vont-elles : x = ${x} et y = ${y}`);
+if (question != true) {
+    while (question != true) {
+        let x = parseFloat(prompt('Entrez une valeur pour la longueur'));
+        let y = parseFloat(prompt('Entrez une valeur pour la largeur'));
+
+        question = confirm(`Ces valeurs vous vont-elles : x = ${x} et y = ${y} ?`);
+    }
+}
+
+function rect(x, y) {
+    return x * y
+}
+
+let surface = document.write(`La surface de votre rectangle est de ${rect(x, y)}m² !`);
