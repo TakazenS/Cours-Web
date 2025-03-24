c37f9be0519f27fc2a748d5483858d012be3bb0a
Loris Moine <loris.moine1402@gmail.com>
2025-03-24 12:48:52 +0100

    Continued Exo 8

--- Docs/TP-8/script/script-exo8-TP8.js
+++ Docs/TP-8/script/script-exo8-TP8.js
@@ -1,25 +1,40 @@
-let surface = pass/*input*/;
+document.getElementById('envoyer').addEventListener('click', calcVolume());
 
-do {
-    let epaisseur = pass/*input*/;
-    if (epaisseur < 15) {
-        window.alert(`Valeur trop faible !`);
-    } else if (epaisseur > 35) {
-        window.alert(`Valeur trop élevée !`);
-    };
-} while (epaisseur < 15 || epaisseur > 35);
+function calcVolume() {
+    let prenom = document.getElementById('prenom');
+    prenom = prenom.value;
 
-const dosage = 350;
-const prixMcube = 91;
+    let nom = document.getElementById('nom');
+    nom = nom.value;
 
-let volumeBeton = parseFloat(surface * epaisser / 100);
+    let surface = document.getElementById('surface');
+    surface = surface.value;
+    
+    let epaisseur = document.getElementById('epaisseur');
+    epaisseur = epaisseur.value;
 
-let qteCiment = (volumeBeton * dosage) / 1000;
+    do {
 
-let nbCamion = parseInt(Math.ceil(volumeBeton / 9));
+        if (epaisseur < 15) {
+            let alert = document.getElementById('formulaire');
+            alert.insertAdjacentElement('beforeend', "<p>L'epaisseur ne doit pas être inferieure à 15cm</p>");
+        } else if (epaisseur > 35) {
+            window.alert(`Valeur trop élevée !`);
+        };
 
-let prixHT = volumeBeton * prixMcube;
+    } while (epaisseur < 15 || epaisseur > 35);
 
-let prixTTC = prixHT + 140 * nbCamion;
+    const dosage = 350;
+    const prixMcube = 91;
 
+    let volumeBeton = parseFloat(surface * epaisser / 100);
+
+    let qteCiment = (volumeBeton * dosage) / 1000;
+
+    let nbCamion = parseInt(Math.ceil(volumeBeton / 9));
+
+    let prixHT = volumeBeton * prixMcube;
+
+    let prixTTC = prixHT + 140 * nbCamion;
+}
 
