abb8c375ca7fac0e5c1cd771faeb1c3d1d704674
Loris Moine <loris.moine1402@gmail.com>
2025-04-17 13:27:33 +0200

    Exercice 1 TP9 Done

--- Docs/TP-9/script/script-exo1-TP9.js
+++ Docs/TP-9/script/script-exo1-TP9.js
@@ -1,4 +1,12 @@
-document.getElementById('link1').addEventListener("mouseenter", () => {
-    console.log('caca')
-})
+const link = document.getElementById('link1');
 
+link.addEventListener('mouseenter', () => {
+    setTimeout(() => {
+        document.getElementById('test1').style.display = "flex";
+    }, 400);
+});
+
+
+link.addEventListener('mouseout', () => {
+    document.getElementById('test1').style.display = "none";
+});
